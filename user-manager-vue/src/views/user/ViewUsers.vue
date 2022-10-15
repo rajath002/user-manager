66ac84d1663afd79b14eb34d219d9ab75ee3ed0a
Rajath <acharya.drajath@gmail.com>
2022-10-15 16:48:42 +0530

    feat(vue): update user infor feature implemented

--- user-manager-vue/src/views/user/ViewUsers.vue
+++ user-manager-vue/src/views/user/ViewUsers.vue
@@ -7,9 +7,32 @@
       <div>{{ user.email }}</div>
       <div>{{ user.createdAt }}</div>
       <div>{{ user.updatedAt }}</div>
+      <div class="update-user" v-if="update.updateId === user._id">
+        <div>
+          <input
+            type="text"
+            name="editUsername"
+            id="updateUsername"
+            v-model="update.userName"
+            placeholder="Name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="updateUseremail"
+            id="updateUseremail"
+            v-model="update.userEmail"
+            placeholder="Email"
+          />
+        </div>
+      </div>
     </div>
     <div>
-      <button @click="updateUser()">Update</button>
+      <button v-if="update.updateId !== user._id" @click="editUser(user)">
+        Edit
+      </button>
+      <button v-else @click="updateUser(user)">Update</button>
       <button @click="deleteUser(user._id)">Delete</button>
     </div>
   </div>
@@ -22,6 +45,12 @@ export default {
     return {
       title: "VIEWS",
       users: [],
+      update: {
+        isUpdate: false,
+        updateId: "",
+        userName: "",
+        userEmail: "",
+      },
     };
   },
   mounted() {
@@ -38,10 +67,46 @@ export default {
     },
     async deleteUser(_id) {
       const response = await userService.deleteUser(_id);
-      if (response.status === 200) {
+      if (response.ok) {
         const deletedUser = await response.json();
-        this.users = this.users.filter(user => user._id !== deletedUser._id);
+        this.users = this.users.filter((user) => user._id !== deletedUser._id);
       }
+    },
+    editUser(user) {
+      if (this.update.isUpdate) {
+        this.resetUpdate();
+        return;
+      }
+      this.update.isUpdate = true;
+      this.update.updateId = user._id;
+      this.update.userName = user.name;
+      this.update.userEmail = user.email;
+    },
+    async updateUser() {
+      const payload = {
+        _id: this.update.updateId,
+        name: this.update.userName,
+        email: this.update.userEmail,
+      };
+      const response = await userService.updateUser(payload);
+      const data = await response.json();
+      this.users = this.users.map((user) => {
+        if (user._id === this.update.updateId) {
+          return {
+            ...user,
+            ...data,
+          };
+        }
+        return user;
+      });
+      this.resetUpdate();
+      console.log("updated user ", data);
+    },
+    resetUpdate() {
+      this.update.isUpdate = false;
+      this.update.updateId = "";
+      this.update.userName = "";
+      this.update.userEmail = "";
     },
   },
 };
@@ -52,6 +117,8 @@ export default {
   display: flex;
   justify-content: space-between;
   width: 400px;
-  margin: 0 auto;
+  margin: 5px auto;
+  padding: 0.5rem;
+  border: #f5f5f5 solid;
 }
 </style>
